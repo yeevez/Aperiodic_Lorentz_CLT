1a054d928e665c858945ec189a1efcb23ea96d3f
jianlong-liu <116191314+jianlong-liu@users.noreply.github.com>
2023-06-13 19:04:14 -0400

    Add files via upload

--- Matlab/scatterer_positions.m
+++ Matlab/scatterer_positions.m
@@ -1,7 +1,10 @@
 function [sp,g,w]=scatterer_positions(rotation,window,grid,center)
+  %creates a Z^3 lattice of size grid/2, at center
   [x,y,z]=ndgrid((-grid/2:grid/2)+round(center(1)),(-grid/2:grid/2)+round(center(2)),(-grid/2:grid/2)+round(center(3)));
   g=[x(:),y(:),z(:)]';
+  %rotates lattice
   p=rotation*g;
+  %finds points within distance window about xy-plane, returns xy-coordinates
   w=find(abs(p(3,:))<window);
   sp=p(1:2,w);
 end
